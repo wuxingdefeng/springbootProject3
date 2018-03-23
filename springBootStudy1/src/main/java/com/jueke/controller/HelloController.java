07a87a62881dc3b4dcf5f478d9d6fd2566f4071f
无形的风 <ouyang_riguang@163.com>
2018-03-23 09:41:09 +0800

    dsf

--- springBootStudy1/src/main/java/com/jueke/controller/HelloController.java
+++ springBootStudy1/src/main/java/com/jueke/controller/HelloController.java
@@ -1,7 +1,6 @@
 package com.jueke.controller;
 
 import com.jueke.expection.BusinessException;
-import com.jueke.rabbit.HelloSender;
 import com.jueke.service.UsersService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -16,15 +15,12 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class HelloController {
     @Autowired
     private UsersService usersService;
-    @Autowired
-    private HelloSender helloSender;
-    @ResponseBody
+
     @RequestMapping("/hello")
     public String hello() throws Exception{
-        helloSender.send("hello发送了22");
-        return "发送成功";
-        //System.out.println("抛出空指针");
-       // throw new NullPointerException("空指针");
+
+        System.out.println("抛出空指针");
+        throw new NullPointerException("空指针");
         //return "Hello World";
     }
     @RequestMapping("/json")
